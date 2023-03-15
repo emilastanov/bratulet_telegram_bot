cae5e1482e703778dc2cc9c83d78b9597c550060
Эмиль Астанов <emilastanov@MBP-Emil.Dlink>
2023-03-15 20:55:31 +0300

    Added github action

---
+++ app/constants/dialogs.py
@@ -0,0 +1,39 @@
+
+last_50_hours_average = "{currency}-{bank}: {price}\n\n<b>Salam alechum, brother!</b>\n" \
+                        "I saw that price on your favorite currency is low " \
+                        "then average of last 50 hours.\n\nI don't know... Do you want to buy it, " \
+                        "may be?\n\n<b>Look this offer:</b>\nCurrency: <b>" \
+                        "{currency}</b>\nBank: <b>" \
+                        "{bank}</b>\nSeller stock: <b>" \
+                        "{seller_stock}</b>\nPrice: <b>{price}</b>\n\n<i>I " \
+                        "think you need to buy it IMHO.</i>"
+
+hello = "Hello {name}!\nNice to meet you."
+
+get_last_50_average = "Done! Now you will receive notification from me about average price" \
+        " of last 50 hours.\nNo thanks needed)"
+
+already_received_this = "Bro, you already get this notification.\nUse <b>/turn_off_notifications</b> " \
+                        "if you want to disable it."
+
+notification_terned_off = "Done! I won't bother you anymore."
+
+again = "Hello {name}!\nNice to see you again)\nIf you need a help - write <b>/help</b>"
+
+help_command = "My dear friend, brother! Helping you, it's an honor for me.\n\n" \
+       "Look at this commands list. I hope this helps you!\n" \
+       "<b>/start</b> - send this is to be my master.\n" \
+       "<b>/get_notifications</b> - send this to be notified.\n" \
+       "<b>/help</b> - i hope this helps you.\n"
+
+active_notifications_does_not_exist = "My friend, i am sorry! May be i forgot about your order... " \
+                                      "Please forgive me, but a dont have information about your notifications. " \
+                                      "You can setting it in main menu. Sorry..."
+
+notification_settings_currencies = "Receive notifications from me - this is the best ever idea, my friend." \
+                                   "Please select needed currency:"
+
+turn_off_notification = "I am sorry. Maybe I was being too obsessive. What notification bothered?"
+
+
+notifications_reactivated = "Ok, my dear friend. The problem successfully solved. Sorry about that)"
